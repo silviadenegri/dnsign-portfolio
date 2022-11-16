a5271f0dcdec3d40e458384ccb26a745dae5e6a1
silviadenegri <97962241+silviadenegri@users.noreply.github.com>
2022-11-16 22:39:03 +0100

    webpack

---
+++ prod/main.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/index.js":
+/*!*********************!*\
+  !*** ./js/index.js ***!
+  \*********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module '@babel/core'\nRequire stack:\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/babel-loader/lib/index.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/loader-runner/lib/loadLoader.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/loader-runner/lib/LoaderRunner.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/lib/NormalModuleFactory.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/lib/Compiler.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/lib/webpack.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/lib/index.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack-cli/lib/webpack-cli.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack-cli/lib/bootstrap.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack-cli/bin/cli.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/bin/webpack.js\n babel-loader@9 requires Babel 7.12+ (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:1029:15)\n    at Function.Module._load (internal/modules/cjs/loader.js:898:27)\n    at Module.require (internal/modules/cjs/loader.js:1089:19)\n    at require (internal/modules/cjs/helpers.js:73:18)\n    at Object.<anonymous> (/Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/babel-loader/lib/index.js:5:11)\n    at Module._compile (internal/modules/cjs/loader.js:1200:30)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1220:10)\n    at Module.load (internal/modules/cjs/loader.js:1049:32)\n    at Function.Module._load (internal/modules/cjs/loader.js:937:14)\n    at Module.require (internal/modules/cjs/loader.js:1089:19)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./js/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: ['./js/index.js', './style.scss'],
+  },
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'prod'),
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    require('postcss-preset-env'),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+
+  // defaults to "web", so only required for webpack-dev-server bug
+  devtool: 'source-map',
+};
