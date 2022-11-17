515e5d3a45d7f9da9b82475b906496d1853d6c6e
silviadenegri <97962241+silviadenegri@users.noreply.github.com>
2022-11-17 02:25:34 +0100

    fix homepage structure and style

--- prod/main.js
+++ prod/main.js
@@ -1,25 +1,40 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./js/index.js":
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
 /*!*********************!*\
   !*** ./js/index.js ***!
   \*********************/
-/***/ (() => {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module '@babel/core'\nRequire stack:\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/babel-loader/lib/index.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/loader-runner/lib/loadLoader.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/loader-runner/lib/LoaderRunner.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/lib/NormalModuleFactory.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/lib/Compiler.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/lib/webpack.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/lib/index.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack-cli/lib/webpack-cli.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack-cli/lib/bootstrap.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack-cli/bin/cli.js\n- /Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/webpack/bin/webpack.js\n babel-loader@9 requires Babel 7.12+ (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:1029:15)\n    at Function.Module._load (internal/modules/cjs/loader.js:898:27)\n    at Module.require (internal/modules/cjs/loader.js:1089:19)\n    at require (internal/modules/cjs/helpers.js:73:18)\n    at Object.<anonymous> (/Users/silviadenegri/Sites/dnsign-portfoliov_v.02/node_modules/babel-loader/lib/index.js:5:11)\n    at Module._compile (internal/modules/cjs/loader.js:1200:30)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1220:10)\n    at Module.load (internal/modules/cjs/loader.js:1049:32)\n    at Function.Module._load (internal/modules/cjs/loader.js:937:14)\n    at Module.require (internal/modules/cjs/loader.js:1089:19)");
+})();
 
-/***/ })
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!********************!*\
+  !*** ./style.scss ***!
+  \********************/
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
 
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./js/index.js"]();
-/******/ 	
+})();
+
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
